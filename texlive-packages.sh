4cf475721d3cd586329cca8eb46d7f50dcc32fad
Son Tu <thaisontunguyen@gmail.com>
2024-01-13 20:37:07 -0500

    add texlive sh file

---
+++ texlive-packages.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo tlmgr install comment xstring printlen cleveref ifmtarg \
+                    polynom needspace pgfplots capt-of changepage \
+                    cancel eulervm tkz-tab \
+                    psnfss frcursive calligra \
+                    placeins gensymb tcolorbox boxedminipage enumitem \
+                    placeins mathpazo
